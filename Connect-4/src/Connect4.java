0724bb75456320e0bcaf30f71b45e9bd3d0a52c3
mlam101 <2004454@learn.vsb.bc.ca>
2026-06-09 11:29:49 -0700

    Refactor Connect4 class to enhance game layout and improve token placement logic

--- Connect-4/src/Connect4.java
+++ Connect-4/src/Connect4.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Connect4 {
@@ -21,7 +23,7 @@ public class Connect4 {
     public Connect4() {
         JFrame frame = new JFrame("Connect 4");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1280, 720));
+        frame.setPreferredSize(new Dimension(1300, 800));
         frame.setResizable(false);
 
         GamePanel panel = new GamePanel();
@@ -39,7 +41,11 @@ public class Connect4 {
         private BufferedImage blueToken;
         private BufferedImage redToken;
         private Integer colourTurn = 1;
-        Integer[][] board = new Integer[7][6];
+        private JPanel buttonPanel;
+        private FlowLayout myLayout;
+        Integer colourWin = 0;
+
+        int[][] board = new int[6][7];
 
 
         private void loadSprite() {           
@@ -61,77 +67,125 @@ public class Connect4 {
         private JButton button6 = new JButton();
         private JButton button7 = new JButton();
 
-        JButton[] buttons = new JButton[] {button1, button2, button3, button4, button5, button6, button7};
+        JButton[] buttons = {button1, button2, button3, button4, button5, button6, button7};
 
 
         JPanel boardPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(boardImg, 200, 50, null);
+                g.drawImage(boardImg, 300, 50, null);
                 drawBoard(g);
+                if (winChecker() != -1) {
+                   drawWin(g);
+                }
             }
         };
 
         private void drawBoard(Graphics g) {
             for (int idx = 0; idx < board.length; idx++) {
-                Integer[] row = board[idx];
+                int[] row = board[idx];
                 for (int cell = 0; cell < row.length; cell++) {
-                    if (row[cell] != null) {
                         switch (row[cell]) {
                             case 1:
-                                g.drawImage(redToken, ((idx*100)+200), ((cell*100)+50), null);
+                                g.drawImage(redToken, ((cell*100)+300), ((idx*100)+50), null);
                                 break;
                             case 2:
-                                g.drawImage(blueToken, ((idx*100)+200), ((cell*100)+50), null);
+                                g.drawImage(blueToken, ((cell*100)+300), ((idx*100)+50), null);
                                 break;
                             default:
                         }
-                    }
-                }   
+                    }  
             }
         }
+
+        private void drawWin(Graphics g) {
+            String message;
+            if (winChecker() == 2) {
+                message = "BLUE WINS";
+            } 
+            else {
+                message = "RED WINS";
+            }
+            g.drawString(message, 300, 50);
+        }
         private void buttonsInit() {
+            buttonPanel = new JPanel();
+            myLayout = new FlowLayout(FlowLayout.LEADING);
+            myLayout.setHgap(0);
+            buttonPanel.setLayout(myLayout);
+            buttonPanel.setOpaque(true);
+            buttonPanel.setVisible(true);
+            JLabel spacerLabel = new JLabel();
+            spacerLabel.setPreferredSize(new Dimension(300, 50));
+            buttonPanel.add(spacerLabel);
             for (int idx = 0; idx < buttons.length; idx++) {
                 JButton button = buttons[idx];
                 button.setName(Integer.toString(idx));
                 button.setPreferredSize(new Dimension(100, 50));
                 button.setBackground(Color.BLUE);
-                boardPanel.add(button);
+                buttonPanel.add(button);
                 button.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        placeToken(Integer.parseInt(button.getName()), colourTurn);
+                        placeToken(Integer.valueOf(button.getName()), colourTurn);
                         boardPanel.repaint();
+                        colourTurn %= 2;
+                        colourTurn++;
                         debugger();
                     }
                     
                 });
             }
         }
-        private void debugger() {
-            for (Integer[] row : board) {
-                for (Integer cell : row) {
-                    System.out.println(cell);
-                }
-            }
-        }
         private void placeToken(Integer num, Integer colour) {
             int i = 5;
-            while ((board[num][i] != null) && (i > 0)) {
+            while ((board[i][num] != 0) && (i > 0)) {
                 i--;
             }
-            board[num][i] = colour;               
-            System.err.println("TOKEN UPDATED");
+            board[i][num] = colour;
+        }
+
+        private void debugger() {
+            for (int[] row : board) {
+                for (int cell : row) {
+                    System.out.print(cell);
+                }
+                System.err.println("    ");
+            }
+        }
+
+        private Integer winChecker() {
+            int checkerNum = 1;
+            while (checkerNum != 3) {
+                for (int idx = 0; idx < board.length; idx++) {
+                    int[] row = board[idx];
+                    for (int i = 0; i < (row.length - 3); i++) {
+                        if (row[i] == checkerNum) {
+                            if (row[i+1] == checkerNum) {
+                                if (row[i+2] == checkerNum) {
+                                    if (row[i+3] == checkerNum) {
+                                        return checkerNum;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            checkerNum++;
+            }
+            return -1;
+            
         }
 
         GamePanel() {
             loadSprite();
-            setPreferredSize(new Dimension(1280, 720));
+            setPreferredSize(new Dimension(1300, 800));
             repaint();
             setLayout(new BorderLayout());
             buttonsInit();
             add(boardPanel, BorderLayout.CENTER);
+            add(buttonPanel, BorderLayout.PAGE_START);
         }
 
     }
